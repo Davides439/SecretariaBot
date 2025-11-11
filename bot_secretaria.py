b246b5606dadc25048b68458470d2195f41b748c
Davides439 <juandavidestrelarosero@gmail.com>
2025-11-10 21:55:05 -0500

    Update bot_secretaria.py

--- bot_secretaria.py
+++ bot_secretaria.py
@@ -1,13 +1,19 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("¡Hola! Soy tu secretaria virtual. ¿En qué puedo ayudarte?")
 
-app = ApplicationBuilder().token(os.getenv("8595014644:AAFItOXlXvpjIKcryCxsLzT9rH5HedTZoe0")).build()
+# Aquí leemos el token guardado en Railway
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+if not TOKEN:
+    raise ValueError("No se encontró la variable TELEGRAM_TOKEN. Configúrala en Railway antes de desplegar.")
+
+app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 
 print("Bot en ejecución...")
 app.run_polling()
-
