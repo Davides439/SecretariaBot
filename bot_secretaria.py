ae16591db211eed0d1a886fad9e50a120e469b99
Davides439 <juandavidestrelarosero@gmail.com>
2025-11-10 21:32:41 -0500

    Update bot_secretaria.py

--- bot_secretaria.py
+++ bot_secretaria.py
@@ -1,12 +1,13 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
+import os
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("¡Hola! Soy tu secretaria virtual. ¿En qué puedo ayudarte?")
 
-app = ApplicationBuilder().token("8595014644:AAFItOXlXvpjIKcryCxsLzT9rH5HedTZoe0").build()
+app = ApplicationBuilder().token(os.getenv("8595014644:AAFItOXlXvpjIKcryCxsLzT9rH5HedTZoe0")).build()
 
 app.add_handler(CommandHandler("start", start))
 
 print("Bot en ejecución...")
 app.run_polling()
+
